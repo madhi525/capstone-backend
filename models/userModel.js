2dc3a3bc7633eb6130fa3205a540ac8aab76ca85
Muhammad Nurul Adhi <muhammadnuruladhi@gmail.com>
2025-06-11 15:12:22 +0700

    implementasi promise di userModel

--- models/userModel.js
+++ models/userModel.js
@@ -1,20 +1,40 @@
-const db = require('../config/db');
+const db = require('../config/dbPromise');
 
 const User = {
-  findByEmail: (email, callback) => {
-    db.query('SELECT * FROM User WHERE email = ?', [email], callback);
+  findByEmail: async (email) => {
+    try {
+      db.query('SELECT * FROM User WHERE email = ?', [email]);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
   },
 
-  create: (user, callback) => {
-    db.query('INSERT INTO User SET ?', user, callback);
+  create: (user) => {
+    try {
+      db.query('INSERT INTO User SET ?', user);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
   },
 
-  updatePassword: (email, hashedPassword, callback) => {
-    db.query('UPDATE User SET password = ? WHERE email = ?', [hashedPassword, email], callback);
+  updatePassword: (email, hashedPassword) => {
+    try {
+      db.query('UPDATE User SET password = ? WHERE email = ?', [hashedPassword, email]);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } 
   },
 
-  updateFotoProfil: (path, email, callback) => {
-    db.query('UPDATE User SET fotoprofil = ? WHERE email = ?', [path,email],callback);
+  updateFotoProfil: (path, email) => {
+    try {
+      db.query('UPDATE User SET fotoprofil = ? WHERE email = ?', [path,email]);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
   }
 };
 
